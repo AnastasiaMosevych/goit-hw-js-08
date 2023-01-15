b8f2380f2ef1aa234b9a74515e98092ad5dce53d
AnastasiaMosevych <ukrainischesmaedel@yahoo.de>
2023-01-15 22:59:53 +0100

    completed 3 tasks

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,34 @@
+import Player from '@vimeo/player';
+
+// Player initializing
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const throttle = require('lodash.throttle');
+
+player.on('play', function(data) {
+    console.log('played the video!');
+});
+
+function storeCurrentTimeInLocalStorage(data) {
+    localStorage.setItem("videoplayer-current-time", data.seconds);
+};
+
+player.on('timeupdate', throttle(storeCurrentTimeInLocalStorage, 1000));
+
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
+
+let currentTime = localStorage.getItem("videoplayer-current-time");
+player.setCurrentTime(currentTime).then(function(seconds) {
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+        break;
+
+        default:
+        break;
+    }
+});
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,51 @@
+const form = document.querySelector(".feedback-form");
+let formData = { email: "", message: "" };
+const emailField = document.querySelector("input[name = 'email']");
+const messageField = document.querySelector("textarea[name ='message']");
+const throttle = require('lodash.throttle');
 
+const throtteledFunction = throttle(function() {
+    localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+}, 500);
+  
+
+form.addEventListener("input", (event) => {
+    if (event.target.name === "email") {
+        formData.email = event.target.value;
+    } else if (event.target.name === "message") {
+        formData.message = event.target.value;
+    };
+
+    throtteledFunction();
+});
+
+window.onload = function () {
+    let storedFormData = null;
+    try { storedFormData = JSON.parse(localStorage.getItem("feedback-form-state")); }
+    catch (error) {
+        console.log(error.name);
+        console.log(error.message);
+    }
+
+    if (storedFormData === null) {
+        return;
+    } else {
+        formData = storedFormData;
+    }
+
+    if (formData.email !== "") {
+        emailField.value = formData.email;
+    } 
+    if (formData.message !== "") {
+        messageField.value = formData.message;
+    }
+};
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    localStorage.clear();
+    form.reset();
+    console.log(formData);
+};
